6f4b60f9392313c41f7224fcbd9bdca7362d828b
mnbf9fca <robert.aleck@cynexia.com>
2020-05-09 16:32:18 +0100

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # outdoor_scales
+
 a simple outdoor scales using 4 x load cells combined with an HX711 and a Particle Photon to route data to the cloud.
+
+## Particle / Arduino code
+
+This code is designed to run on the Particle Photon device. It makes use of the great HX711ADC library from <https://github.com/eliteio/HX711ADC/>
+
+It assumes:
+
+- HX711 DOUT / DT is connected to pin D3
+- HX711 SCK is connected to pin D2
+
+The LED blinks every time a reading is taken.
---
+++ particle/src/particle.ino
@@ -0,0 +1,109 @@
+// This #include statement was automatically added by the Particle IDE.
+#include <HX711ADC.h>
+
+/*
+ * Project particle
+ * Description:
+ * Author:
+ * Date:
+ */
+
+#define HX711DOUT D3
+#define HX711SCK D2
+int led1 = D0; // Instead of writing D0 over and over again, we'll write led1
+// You'll need to wire an LED to this one to see it blink.
+
+int led2 = D7; // Instead of writing D7 over and over again, we'll write led2
+
+/**
+ * helper function to publish integers e.g. for debugging
+ */
+void publishNumber(const char *name, int integer)
+{
+  publishNumber(name, (float)integer);
+}
+
+void publishNumber(const char *name, double value)
+{
+  char *buf;
+  size_t bufz = snprintf(NULL, 0, "%f", value) * sizeof(char);
+  buf = (char *)malloc(bufz);
+  if (buf == NULL)
+  {
+    publishChar("Out of memory", "publishNumber");
+    return;
+  }
+  snprintf(buf, bufz, "%f", value);
+  publishChar(name, buf);
+  free(buf);
+}
+void publishNumber(const char *name, long int value)
+{
+  char *buf;
+  size_t bufz = snprintf(NULL, 0, "%ld", value) * sizeof(char);
+  buf = (char *)malloc(bufz);
+  if (buf == NULL)
+  {
+    publishChar("Out of memory", "publishNumber");
+    return;
+  }
+  snprintf(buf, bufz, "%f", value);
+  publishChar(name, buf);
+  free(buf);
+}
+/**
+ * helper function to publish chars e.g. for debugging
+ */
+void publishChar(const char *name, const char *message)
+{
+  Particle.publish(name, message, 0, PRIVATE);
+  delay(1000); // ensure not throttled
+}
+
+HX711ADC scale(HX711DOUT, HX711SCK);
+
+double currentValue = 0.0;
+
+// setup() runs once, when the device is first turned on.
+void setup()
+{
+
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  scale.begin();
+  publishChar("Before setting up the scale:", "");
+  publishNumber("read:", scale.read()); // print a raw reading from the ADC
+
+  publishNumber("read average:", scale.read_average(20)); // print the average of 20 readings from the ADC
+
+  publishNumber("get value:", scale.get_value(5)); // print the average of 5 readings from the ADC minus the tare weight (not set yet)
+
+  publishNumber("get units:", scale.get_units(5)); // print the average of 5 readings from the ADC minus tare weight (not set) divided
+                                                   // by the SCALE parameter (not set yet)
+
+  scale.set_scale(2280.f); // this value is obtained by calibrating the scale with known weights; see the README for details
+  scale.tare();            // reset the scale to 0
+
+  publishChar("After setting up the scale:", "");
+
+  publishNumber("read:", scale.read()); // print a raw reading from the ADC
+
+  publishNumber("read average:", scale.read_average(20)); // print the average of 20 readings from the ADC
+
+  publishNumber("get value:", scale.get_value(5)); // print the average of 5 readings from the ADC minus the tare weight, set with tare()
+
+  publishNumber("get units:", scale.get_units(5)); // print the average of 5 readings from the ADC minus tare weight, divided
+                                                   // by the SCALE parameter set with set_scale
+
+  publishChar("Readings:", "");
+}
+void loop()
+{
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH); // put the ADC in sleep mode
+  publishNumber("one reading:", scale.get_units());
+  publishNumber("| average:", scale.get_units(10));
+  delay(100);
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+}
