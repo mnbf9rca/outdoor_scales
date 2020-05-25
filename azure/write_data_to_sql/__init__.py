3a4e4a4437e73e38a4060bd88d303143df2d0775
mnbf9fca <robert.aleck@cynexia.com>
2020-05-25 21:20:52 +0100

    tweaks

--- azure/write_data_to_sql/__init__.py
+++ azure/write_data_to_sql/__init__.py
@@ -9,6 +9,9 @@ from dateutil import parser
 
 
 def main(event: func.EventHubEvent):
+
+    # for some reason, when executing in azure, we dont get an iterable, even with cardinality=many
+    # so test for it
     if isinstance(event, Iterable):
         logging.info('iterable')
         event_data = [loads(s) for s in [e.get_body().decode('utf-8') for e in event]]
@@ -20,37 +23,28 @@ def main(event: func.EventHubEvent):
                  dumps(event_data))
 
     cnxn = pyodbc.connect(environ.get('cloud_sql_conn_string'))
-    cursor = cnxn.cursor()
-    cursor.fast_executemany = True
-    ''' 
-    table: cloud_scales_source_data
-    [source] varchar(64),
-    [type] varchar(64),
-    [name] varchar(64),
-    [event] varchar(64),
-    [data] decimal(18,0),
-    [device_id] varchar(24),
-    [published_at] datetime,
-    '''
+    with cnxn:
+        cursor = cnxn.cursor()
+        cursor.fast_executemany = True
+        ''' 
+        table: cloud_scales_source_data
+        [source] varchar(64),
+        [type] varchar(64),
+        [name] varchar(64),
+        [event] varchar(64),
+        [data] decimal(18,0),
+        [device_id] varchar(24),
+        [published_at] datetime,
+        '''
 
-    cursor.executemany("""
-    INSERT INTO cloud_scales_source_data
-    ( [source], 
-      [type], 
-      [name], 
-      [event], 
-      [data], 
-      [device_id], 
-      [published_at])
-    VALUES (?,?,?,?,?,?,?)    
-    """, [(s["source"],
-           s["type"],
-           s["name"],
-           s["event"],
-           s["data"],
-           s["device_id"],
-           parser.parse(s["published_at"]))
-          for s in event_data])
-    cursor.commit()
+        cursor.executemany("""INSERT INTO cloud_scales_source_data
+                        (       [source],   [type],   [name],    [event],    [data],    [device_id],                  [published_at])
+                        VALUES (?,?,?,?,?,?,?)    
+                        """,
+                           [(s["source"], s["type"], s["name"], s["event"], s["data"], s["device_id"], parser.parse(s["published_at"]))
+                            for s in event_data])
+
+    # shouldnt need to close, but seems to improve throughput
+    cnxn.close()
 
     return
