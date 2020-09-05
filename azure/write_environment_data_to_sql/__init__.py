60dc45f79c3b3592ab97ae790d72ea3d037bca2a
mnbf9fca <robert.aleck@cynexia.com>
2020-09-05 18:07:50 +0100

    bail on error

--- azure/write_environment_data_to_sql/__init__.py
+++ azure/write_environment_data_to_sql/__init__.py
@@ -2,6 +2,7 @@ import logging
 from collections.abc import Iterable
 from json import dumps, loads
 from os import environ
+from typing import List
 
 import azure.functions as func
 import pyodbc
@@ -13,18 +14,26 @@ def main(event: func.EventHubEvent):
 
     # for some reason, when executing in azure, we dont always get an iterable, even with cardinality=many
     # so test for it
-    if isinstance(event, Iterable):
-        logger.debug('iterable')
-        event_data = [loads(s) for s in [e.get_body().decode('utf-8') for e in event]]
-    else:
-        logger.debug('not iterable')
-        event_data = loads(event.get_body().decode('utf-8'))
+    event_data = _get_event_data(event)
     logger.info(f'Received trigger for {len(event_data)} items')
     logger.debug(f'payload: {dumps(event_data)}')
+
+    return _write_to_database(event_data)
+
+def _get_event_data(event: func.EventHubEvent) -> List:
+    if isinstance(event, Iterable):
+        logger.debug('iterable')
+        return [loads(s) for s in [e.get_body().decode('utf-8') for e in event]]
+    else:
+        logger.debug('not iterable')
+        return loads(event.get_body().decode('utf-8'))
+
+def _write_to_database(event_data: dict) -> int:
     try:
         cnxn = pyodbc.connect(environ.get('cloud_sql_conn_string'))
     except Exception as e:
             logger.error(e)
+            return -1
     try:
         with cnxn:
             cursor = cnxn.cursor()
@@ -53,8 +62,9 @@ def main(event: func.EventHubEvent):
                                     for s in event_data])
     except Exception as e:
         logger.error(e)
+        return -1
     # shouldnt need to close, but seems to improve throughput
     cnxn.close()
     logger.info("Successfully stored in DB")
 
-    return
+    return 0
